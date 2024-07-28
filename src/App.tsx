190095f0e76b5e084e7c374762d965e2f5eef54c
kshiteej <kshiteej@groomit.me>
2024-07-28 18:42:51 +0530

    Content

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import SendWhatsappMessage from './features/SendWhatsappMessage/SendWhatsappMessage';
 import SendEmail from './features/SendEmail/SendEmail';
 import PaymentGateway from './features/PaymentGateway/PaymentGateway';
+import AboutOlympiad from './features/AboutOlympiad/AboutOlympiad';
 
 import './App.css';
+
 
 const App = () => {
   return (
     <>
       <h1>Olympiad</h1>
       <SendWhatsappMessage />
-     <SendEmail />
-     <PaymentGateway />
+      <SendEmail />
+      <PaymentGateway />
+      <AboutOlympiad />
     </>
   );
 };
